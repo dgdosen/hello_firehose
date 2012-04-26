8001392baa4f75de9ae021e59886dc516c6080d4
Daniel Dosen <ddosen@gmail.com>
2012-04-25 17:53:34 -0700

    adding a firehouse based rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloFirehose::Application.config.secret_token = 'b91f30e31956b5b434673a04e1e5b4a0d60f6a91a7402721bb5fc52f803cf81ba44e6ecdc64673d9f80b80307775ab10a603e3575cd0baea38042a944b76a4d5'
